f9ae8655244f3b3542edd5e6aca998fa8a106cf4
JK <jinhh999@gmail.com>
2017-02-25 16:37:37 +0800

    gulp的js合并压缩和混淆

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,13 @@ gulp.task('style', function() {
         .pipe(cssnano())
         .pipe(gulp.dest('dist/style/'));
 });
+//2.js合并，压缩，混淆
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('script', function() {
+    return gulp.src('src/script/*.js')
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/script/'));
+});
